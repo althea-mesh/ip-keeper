da26fb77cb15c7e2f42bc017b49d54b223f079a4
Jehan <jehan.tremback@gmail.com>
2017-06-21 16:34:15 -0700

    more bugfixes and tests

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/binary"
 	"errors"
-	"fmt"
+	"log"
 	"net"
+
+	"fmt"
 
 	"github.com/apparentlymart/go-cidr/cidr"
 	"github.com/boltdb/bolt"
@@ -22,10 +24,16 @@ import (
 // Release IP
 //
 
-func BytesToPublicKey(bytes []byte) [32]byte {
+func bytesToPublicKey(bytes []byte) [32]byte {
 	var publicKey [32]byte
 	copy(publicKey[:], bytes)
 	return publicKey
+}
+
+func makeUvarint(n uint64) []byte {
+	b := make([]byte, 10)
+	i := binary.PutUvarint(b, n)
+	return b[:i]
 }
 
 type IPRecord struct {
@@ -35,7 +43,69 @@ type IPRecord struct {
 }
 
 func main() {
-	fmt.Println("hop")
+	// Open the my.db data file in your current directory.
+	// It will be created if it doesn't exist.
+	db, err := bolt.Open("ipKeeper.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		db.Close()
+	}()
+
+	Init(db, "10.0.0.0/16", 2)
+
+	ip, err := GetIP(db, [32]byte{0x3b, 0xee, 0xb8, 0xd0, 0x2, 0x7c, 0x31, 0x38, 0x1a, 0xc2, 0x28, 0xdc, 0xe1, 0x23, 0x2d, 0x62, 0x9c, 0xcd, 0x68, 0x1e, 0xde, 0x7d, 0x45, 0xbb, 0xc0, 0xec, 0x10, 0x87, 0x94, 0x8d, 0xfe, 0xa})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ip)
+
+	ip, err = GetIP(db, [32]byte{0x4b, 0xee, 0xb8, 0xd0, 0x2, 0x7c, 0x31, 0x38, 0x1a, 0xc2, 0x28, 0xdc, 0xe1, 0x23, 0x2d, 0x62, 0x9c, 0xcd, 0x68, 0x1e, 0xde, 0x7d, 0x45, 0xbb, 0xc0, 0xec, 0x10, 0x87, 0x94, 0x8d, 0xfe, 0xa})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ip)
+
+	ip, err = GetIP(db, [32]byte{0x2b, 0xee, 0xb8, 0xd0, 0x2, 0x7c, 0x31, 0x38, 0x1a, 0xc2, 0x28, 0xdc, 0xe1, 0x23, 0x2d, 0x62, 0x9c, 0xcd, 0x68, 0x1e, 0xde, 0x7d, 0x45, 0xbb, 0xc0, 0xec, 0x10, 0x87, 0x94, 0x8d, 0xfe, 0xa})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ip)
+
+	ip, err = GetIP(db, [32]byte{0x2b, 0xee, 0xb8, 0xd0, 0x2, 0x7c, 0x31, 0x38, 0x1a, 0xc2, 0x28, 0xdc, 0xe1, 0x23, 0x2d, 0x62, 0x9c, 0xcd, 0x68, 0x1e, 0xde, 0x7d, 0x45, 0xbb, 0xc0, 0xec, 0x10, 0x87, 0x94, 0x8d, 0xfe, 0xa})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ip)
+}
+
+func Init(db *bolt.DB, parentRange string, newBits int) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		config, err := tx.CreateBucket([]byte("Config"))
+		_, err = tx.CreateBucket([]byte("PubKey<->Number"))
+		_, err = tx.CreateBucket([]byte("AvailableList"))
+		if err != nil {
+			return err
+		}
+
+		err = config.Put([]byte("ParentRange"), []byte(parentRange))
+		if err != nil {
+			return err
+		}
+
+		err = config.Put([]byte("NewBits"), makeUvarint(uint64(newBits)))
+		if err != nil {
+			return err
+		}
+
+		err = config.Put([]byte("RangeSequence"), makeUvarint(uint64(0)))
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
 }
 
 func GetIP(db *bolt.DB, pubkey [32]byte) (*net.IPNet, error) {
@@ -57,7 +127,7 @@ func GetIP(db *bolt.DB, pubkey [32]byte) (*net.IPNet, error) {
 			return errors.New("Missing NewBits from config")
 		}
 
-		newBits, _ := binary.Varint(n)
+		newBits, _ := binary.Uvarint(n)
 
 		num, err := getIPNumber(tx, pubkey, parent)
 		if err != nil {
@@ -81,8 +151,6 @@ func getIPNumber(tx *bolt.Tx, pubkey [32]byte, parent *net.IPNet) (uint64, error
 	config := tx.Bucket([]byte("Config"))
 	avail := tx.Bucket([]byte("AvailableList"))
 
-	var num uint64
-
 	// Try to find existing entry for key
 	n := key2num.Get(pubkey[:])
 	if n != nil {
@@ -105,12 +173,12 @@ func getIPNumber(tx *bolt.Tx, pubkey [32]byte, parent *net.IPNet) (uint64, error
 		return 0, errors.New("Missing RangeSequence from config")
 	}
 
-	num, _ = binary.Uvarint(n)
+	num, _ := binary.Uvarint(n)
+	// Last address in range is broadcast address
+	hostMax := cidr.AddressCount(parent)
 
-	if num+1 < cidr.AddressCount(parent) {
-		var b []byte
-		binary.PutUvarint(b, num+1)
-		config.Put([]byte("RangeSequence"), b)
+	if num < hostMax {
+		config.Put([]byte("RangeSequence"), makeUvarint(uint64(num+1)))
 		return num, nil
 	}
 	return 0, errors.New("No available IPs in range")
